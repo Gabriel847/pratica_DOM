4a09890e89b570526227566405cd78a8731bf76f
Gabriel da Silva <42205864+Gabriel847@users.noreply.github.com>
2020-02-14 17:35:00 -0300

    Add files via upload
    Terminado \o/

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function q1(){
+//1ª
 	//A)
 		document.getElementById('letraAzul').style="color: blue;"
 	//B)
@@ -16,23 +17,81 @@ function q1(){
 }
 
 function q2(){
-//2º
+//2ª
 	const quant = prompt("Quantidade para a matriz NxN");	
-	let matriz = new Array();
-	for (let i = 0; i <= quant; i++) {
+	let vet = new Array(),vet2 = new Array();
+	for (let i = 0; i < quant; i++) {
 		for (let j = 0; j <= quant; j++) {
-			matriz = [[i],[j]];
+			vet[i] = i;
+			vet2[j] = j;
 		}
 	}
 
-	for (let i = 0; i <= quant; i++) {
-		for (let j = 0; j <= quant; j++) {
-			matriz = [[i],[j]];
+	for (i = 0; i < quant; i++) {		
+		for (let j = 0; j < quant; j++) {
+			document.write("<label style='border: solid 1px black;'>"+vet[i]+"  "+vet2[j]+"</label>");
 		}
+		document.write("<br>");
 	}
 	
 }
 
-q2()
+function q3(){
+//3ª
+	const quant = prompt("Quantidade para a matriz NxN");	
+	let vet = new Array(),vet2 = new Array();
+	for (let i = 0; i < quant; i++) {
+		for (let j = 0; j <= quant; j++) {
+			vet[i] = i;
+			vet2[j] = j;
+		}
+	}
 
-	
+	for (i = 0; i < quant; i++) {		
+		for (let j = 0; j < quant; j++) {
+			if(i == j){
+				document.write("<label class='diag1'>"+vet[i]+"  "+vet2[j]+"</label>");
+			}else{
+				document.write("<label style='border: solid 1px black;'>"+vet[i]+"  "+vet2[j]+"</label>");
+			}
+		}
+		document.write("<br>");
+	}
+	let labels = document.getElementsByClassName("diag1");
+	for (let i = 0; i < labels.length; i++) {
+	  labels[i].style="color: orange;border: solid 1px orange";
+	}	
+}
+
+//4ª
+function q4(){
+	const quant = prompt("Quantidade para a matriz NxN");	
+	let vet = new Array(),vet2 = new Array();
+	for (let i = 0; i < quant; i++) {
+		for (let j = 0; j <= quant; j++) {
+			vet[i] = i;
+			vet2[j] = j;
+		}
+	}
+
+	for (i = 0; i < quant; i++) {		
+		for (j = 0; j < quant; j++) {
+			if((i+j) == quant-1){
+				document.write("<label class='diag2'>"+vet[i]+"  "+vet2[j]+"</label>");
+			}else{
+				document.write("<label style='border: solid 1px black;'>"+vet[i]+"  "+vet2[j]+"</label>");
+			}
+		}
+		document.write("<br>");
+	}
+	let labels2 = document.getElementsByClassName("diag2");
+	for (let i = 0; i < labels2.length; i++) {
+	  labels2[i].style="color: orange;border: solid 1px orange";
+	}
+}
+//5ª
+//a)SIM, será null. Pois ao trazer seu último filho, a função tenta trzer seu último irmão, porém não existente pois na situação o elemento é filho único
+//b)SIM, será null. Como foi pego o primeiro elemento da lista e tenta se usar uma função previousSibling, que busca o anterior, acaba-se por ter um null pois não existe elem.children[-1]
+
+//6º Primeiramente não iria ser considerado uma função válida, já que o modo d e trabalhar com camadas é através de funções. Segundo que o jeito mais correto são com funções de coleta de tags filhas
+
